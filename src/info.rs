15dedf9c05d8761dc4fb0977e0df7c3b4fbb19ef
Toxikuu <toxikuu@users.noreply.github.com>
2025-03-07 01:02:07 -0600

    error handling is a feature now (also incremented version)

--- src/info.rs
+++ src/info.rs
@@ -1,15 +1,23 @@
+#[cfg(feature = "error_handling")]
 use anyhow::{Context, Result};
 use std::{
     fs::read_to_string,
     env::var,
 };
 
+#[cfg(feature = "error_handling")]
 fn title() -> Result<String> {
     Ok(
         var("USER")? + "@" + read_to_string("/etc/hostname")?.trim()
     )
 }
 
+#[cfg(not(feature = "error_handling"))]
+fn title() -> String {
+    var("USER").unwrap() + "@" + read_to_string("/etc/hostname").unwrap().trim()
+}
+
+#[cfg(feature = "error_handling")]
 fn kernel() -> Result<String> {
     Ok(
         read_to_string("/proc/version")?
@@ -20,13 +28,37 @@ fn kernel() -> Result<String> {
     )
 }
 
-fn shell() -> Result<String> {
-    var("SHELL")?
-        .rsplit_once('/')
-        .map(|(_, sh)| sh.to_string())
-        .context("Invalid $SHELL")
+#[cfg(not(feature = "error_handling"))]
+fn kernel() -> String {
+    read_to_string("/proc/version")
+        .unwrap()
+        .split_whitespace()
+        .nth(2)
+        .unwrap()
+        .to_string()
 }
 
+#[cfg(feature = "error_handling")]
+fn shell() -> Result<String> {
+    let shell = var("SHELL")?;
+    Ok(
+        shell
+            .rsplit_once('/')
+            .map(|(_, sh)| sh.to_string())
+            .unwrap_or(shell)
+    )
+}
+
+#[cfg(not(feature = "error_handling"))]
+fn shell() -> String {
+    let shell = var("SHELL").unwrap();
+    shell
+        .rsplit_once('/')
+        .map(|(_, sh)| sh.to_string())
+        .unwrap_or(shell)
+}
+
+#[cfg(feature = "error_handling")]
 fn distro() -> Result<String> {
     read_to_string("/etc/os-release")?
         .lines()
@@ -36,6 +68,18 @@ fn distro() -> Result<String> {
         .context("Failed to parse pretty name")
 }
 
+#[cfg(not(feature = "error_handling"))]
+fn distro() -> String {
+    read_to_string("/etc/os-release")
+        .unwrap()
+        .lines()
+        .find(|l| l.starts_with("PRETTY_NAME="))
+        .and_then(|l| l.split_once('='))
+        .map(|(_, v)| v.trim_matches('"').to_string())
+        .unwrap()
+}
+
+#[cfg(feature = "error_handling")]
 pub fn all() -> Result<[String; 4]> {
     Ok([
         format!("\x1b[31;1m{}"    , title()?),
@@ -43,4 +87,14 @@ pub fn all() -> Result<[String; 4]> {
         format!("\x1b[36;1mkr  {}", kernel()?),
         format!("\x1b[36;1msh  {}", shell()?),
     ])
+}
+
+#[cfg(not(feature = "error_handling"))]
+pub fn all() -> [String; 4] {
+    [
+        format!("\x1b[31;1m{}"    , title()),
+        format!("\x1b[36;1mos  {}", distro()),
+        format!("\x1b[36;1mkr  {}", kernel()),
+        format!("\x1b[36;1msh  {}", shell()),
+    ]
 }
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,16 @@ const CAT: [&str; 4] =
 "\x1b[37m  じしˍ,)ノ",
 ];
 
+#[cfg(feature = "error_handling")]
 fn main() {
-    CAT.iter().zip(info::all().expect(":(")).for_each(|(c, i)| {
+    CAT.iter().zip(info::all().expect("Something went wrong")).for_each(|(c, i)| {
+        println!("{c}  {i}\x1b[0m")
+    });
+}
+
+#[cfg(not(feature = "error_handling"))]
+fn main() {
+    CAT.iter().zip(info::all()).for_each(|(c, i)| {
         println!("{c}  {i}\x1b[0m")
     });
 }
