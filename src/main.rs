b448cbf5846836b0718f620656b7ffafbcf46121
Toxikuu <toxikuu@users.noreply.github.com>
2025-05-07 03:43:19 -0500

    added version flag

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod info;
 
+use std::env::args;
+use std::process::exit;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const NAME: &str = env!("CARGO_PKG_NAME");
+
 const CAT: [&str; 4] = [
     "\x1b[37m  ╱|、     ",
     "\x1b[37m (˚ˎ。7    ",
@@ -9,7 +15,7 @@ const CAT: [&str; 4] = [
 
 #[cfg(feature = "error_handling")]
 fn main() {
-    use std::process::exit;
+    show_version();
 
     let Ok(info) = info::all().map_err(|e| eprintln!("Error: {e}")) else {
         exit(1)
@@ -22,7 +28,16 @@ fn main() {
 
 #[cfg(not(feature = "error_handling"))]
 fn main() {
+    show_version();
+
     CAT.iter()
         .zip(info::all())
         .for_each(|(c, i)| println!("{c}  {i}\x1b[0m"));
+}
+
+fn show_version() {
+    if args().any(|a| matches!(a.as_str(), "-V" | "--version")) {
+        println!("{NAME} {VERSION}");
+        exit(0)
+    }
 }
