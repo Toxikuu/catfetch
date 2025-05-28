e6d9d0c137af11afc719af2f43e3995de9dbb407
Toxikuu <toxikuu@users.noreply.github.com>
2025-05-28 02:20:57 -0500

    better fallbacks

--- src/info.rs
+++ src/info.rs
@@ -1,27 +1,42 @@
 use anyhow::{Context, Result};
 use std::{env::var, fs::read_to_string, process::Command};
 
-fn get_user() -> Result<String> {
-    if let Ok(user) = var("USER") {
-        Ok(user)
+fn cmd(parts: &[&str]) -> Option<String> {
+    let out = Command::new(parts.first().unwrap())
+        .args(parts.iter().skip(1))
+        .output()
+        .ok()?
+        .stdout;
+    let out = out.trim_ascii();
+
+    if out.is_empty() {
+        None
     } else {
-        Ok(String::from_utf8_lossy(
-            Command::new("whoami")
-                .output()
-                .context("Couldn't detect user")?
-                .stdout
-                .trim_ascii(),
-        )
-        .to_string())
+        Some(String::from_utf8_lossy(out).to_string())
     }
 }
 
-fn title() -> Result<String> {
-    Ok(get_user()?
-        + "@"
-        + read_to_string("/etc/hostname")
-            .context("Couldn't read /etc/hostname")?
-            .trim())
+fn user() -> Option<String> {
+    var("USER")
+        .ok()
+        .map(|s| s.trim_ascii().to_owned())
+        .map_or(cmd(&["whoami"]), Some)
+}
+
+fn host() -> Option<String> {
+    if let Ok(h) = read_to_string("/etc/hostname") {
+        Some(h.trim_ascii().to_owned())
+    } else if let Ok(h) = var("HOSTNAME") {
+        Some(h.trim_ascii().to_owned())
+    } else {
+        cmd(&["hostname"]).map_or_else(|| cmd(&["uname", "-r"]), Some)
+    }
+}
+
+fn title() -> String {
+    let user = user().unwrap_or("unknown".to_owned());
+    let host = host().unwrap_or("unknown".to_owned());
+    format!("{user}@{host}")
 }
 
 fn kernel() -> Result<String> {
@@ -30,28 +45,21 @@ fn kernel() -> Result<String> {
         .split_whitespace()
         .nth(2)
         .context("Failed to get kernel version")?
-        .to_string())
+        .to_owned())
 }
 
-fn shell() -> Result<String> {
+fn shell() -> Option<String> {
     let shell = if let Ok(shell) = var("SHELL") {
-        shell
+        Some(shell)
     } else {
-        String::from_utf8_lossy(
-            Command::new("readlink")
-                .arg("/bin/sh")
-                .output()
-                .expect("Couldn't detect shell")
-                .stdout
-                .trim_ascii(),
-        )
-        .to_string()
+        cmd(&["readlink", "/bin/sh"])
     };
 
-    Ok(shell
-        .rsplit_once('/')
-        .map(|(_, sh)| sh.to_string())
-        .unwrap_or(shell))
+    shell.map(|sh| {
+        sh.rsplit_once('/')
+            .map(|(_, sh)| sh.to_owned())
+            .unwrap_or(sh)
+    })
 }
 
 fn distro() -> Result<String> {
@@ -60,15 +68,25 @@ fn distro() -> Result<String> {
         .lines()
         .find(|l| l.starts_with("PRETTY_NAME="))
         .and_then(|l| l.split_once('='))
-        .map(|(_, v)| v.trim_matches('"').to_string())
+        .map(|(_, v)| v.trim_matches('"').to_owned())
         .context("Failed to parse pretty name")
 }
 
 pub fn all() -> Result<[String; 4]> {
     Ok([
-        format!("\x1b[31;1m{}", title()?),
-        format!("\x1b[36;1mos  {}", distro()?),
-        format!("\x1b[36;1mkr  {}", kernel()?),
-        format!("\x1b[36;1msh  {}", shell()?),
+        format!("\x1b[31;1m{}", title()),
+        format!(
+            "\x1b[36;1mos  {}",
+            distro()
+                .map_err(|e| eprintln!("Failed to get distro: {e}"))
+                .unwrap_or("unknown".to_owned())
+        ),
+        format!(
+            "\x1b[36;1mkr  {}",
+            kernel()
+                .map_err(|e| eprintln!("Failed to get kernel: {e}"))
+                .unwrap_or("unknown".to_owned())
+        ),
+        format!("\x1b[36;1msh  {}", shell().unwrap_or("unknown".to_owned())),
     ])
 }
